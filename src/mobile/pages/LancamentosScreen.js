5f5447515a675f7c6abfee8061daeb8c08bc55a0
Vanessa <vanessa.chaves@sga.pucminas.br>
2024-06-02 21:44:55 -0300

    lancamentos

--- src/mobile/pages/LancamentosScreen.js
+++ src/mobile/pages/LancamentosScreen.js
@@ -1,1 +1,23 @@
+import { View, Text, StyleSheet } from 'react-native';
 
+const LancamentosScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Lançamentos Page</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.7)',
+  },
+  text: {
+    fontSize: 24,
+  },
+});
+
+export default LancamentosScreen;
